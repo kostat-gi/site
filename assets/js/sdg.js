414bcde2313d4df378a6aa660922320a6d53b9c8
kostat-gi <103187784+kostat-gi@users.noreply.github.com>
2022-10-14 01:23:36 +0000

    Deploying to gh-pages from  @ cadda28f07e12f91b069038196532ba59766fd2f 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -3759,7 +3759,8 @@ var indicatorView = function (model, options) {
           'class': 'btn btn-primary btn-download',
           'tabindex': 0
         });
-      var blob = new Blob([tableCsv], {
+      //var blob = new Blob([tableCsv], {
+      var blob = new Blob(["\ufeff"+tableCsv], {
         type: 'text/csv;charset=utf8'
       });
       if (window.navigator && window.navigator.msSaveBlob) {
